a372c45af7b9bbfb43b739d3807198a9d8b71e19
Ahmed <ahmedchouihi@gmail.com>
2021-05-08 20:39:34 +0100

    'addComponentMaterielsEntree'

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from "./components/navbar.component";
 import MaterielsList from "./components/materiels-list.component";
 import EditMateriel from "./components/edit-materiels.component";
 import CreateMateriel from "./components/create-materiel.component";
+import MaterielsEntree from "./components/materiels-list-entree.component"
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/" exact component={MaterielsList}/>
                 <Route path="/edit/:id" component={EditMateriel}/>
                 <Route path="/create" component={CreateMateriel}/>
+                <Route path="/entree" component={MaterielsEntree}/>
             </div>
         </Router>
     );
